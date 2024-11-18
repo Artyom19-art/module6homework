038dc8d9a59afc4fbfcde653273955494d764a3d
Artyom19-art <artyomoganesian@yandex.ru>
2024-11-18 23:15:03 +0300

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -1,60 +1,47 @@
 class Animal:
-    live = True
-    sound = None
-    _DEGREE_OF_DANGER = 0
-
-    def __init__(self, speed):
-        self._cords = [0, 0, 0]
-        self.speed = speed
-
-    def move(self, dx, dy, dz):
-        if dz < 0 and self._cords[2] + dz < 0:
-            print("It's too deep, i can't dive :(")
-        else:
-            self._cords[0] += dx * self.speed
-            self._cords[1] += dy * self.speed
-            self._cords[2] += dz * self.speed
-
-    def get_cords(self):
-        print(f"X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}")
+    def __init__(self, x, y, z):
+        self.live = True
+        self._cords = {"X": x, "Y": y, "Z": z}
 
     def attack(self):
-        if self._DEGREE_OF_DANGER < 5:
-            print("Sorry, i'm peaceful :)")
-        else:
-            print("Be careful, i'm attacking you 0_0")
+        print("Be careful, I'm attacking you 0_0")
 
+    def move(self, dx, dy, dz):
+        self._cords["X"] += dx
+        self._cords["Y"] += dy
+        self._cords["Z"] += dz
+
+    def get_cords(self):
+        print(f"X: {self._cords['X']} Y: {self._cords['Y']} Z: {self._cords['Z']}")
 
 class Bird(Animal):
-    beak = True
-
-    def lay_eggs(self):
-        import random
-        num_eggs = random.randint(1, 4)
-        print(f"Here are(is) {num_eggs} eggs for you")
+    def speak(self):
+        print("Tweet-tweet")
 
 class AquaticAnimal(Animal):
-    _DEGREE_OF_DANGER = 3
-
     def dive_in(self, dz):
         dz = abs(dz)
-        self._cords[2] -= dz * self.speed * 2
+        self._cords["Z"] -= dz*2
 
 class PoisonousAnimal(Animal):
     _DEGREE_OF_DANGER = 8
 
 class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
-    sound = "Click-click-click"
+    def __init__(self, x, y=20, z=30):
+        super().__init__(x, y, z)
+        self.beak = True
+        self.sound = "Click-click-click"
 
-    def __init__(self, speed):
-        super().__init__(speed)
+    def lay_eggs(self):
+        num_eggs = 3
+        print(f"Here are(is) {num_eggs} eggs for you")
 
 db = Duckbill(10)
 
 print(db.live)
 print(db.beak)
 
-db.sound
+db.speak()
 db.attack()
 
 db.move(1, 2, 3)
